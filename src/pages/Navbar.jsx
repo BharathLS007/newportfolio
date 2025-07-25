934aaef1397b2f06fd331f54d638ebd2dab9421c
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-25 17:26:03 +0530

    navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
-    this is nav
-
-    </div>
+    
+        <div className="navbar">
+            <div className="logo">Front<span>developer</span></div>
+             <div className="nav">
+              <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/About'>About</Link></li>
+                <li><Link to='/Projects'>Projects</Link></li>
+                <li><Link to='/Contact'>Contact</Link></li>
+              </ul>
+             </div>
+        </div>
+     
   );
 }
 
