501a9905e95971875af812fb83b633e304750ceb
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-25 17:42:48 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,17 @@ import Navbar from './Navbar'
 
 function Home() {
   return (
-    <div>
-          
+<body>
+      <div>          
         <Navbar/>
+
+        <div className="container">
+          <h1 className='heading'>Hi There! <span className='wavename'>👋</span></h1>
+          <h1 className='heading'>I'M <span>Bharath L S</span></h1>
+          <h1>s</h1>
+        </div>
     </div>
+</body>
   )
 }
 
